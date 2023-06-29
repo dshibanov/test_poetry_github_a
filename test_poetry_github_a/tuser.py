9227f3d9056c1da556c677e1321aef97a093695f
Dmitrii 108 <dmitry.shibanov@gmail.com>
2023-06-29 15:48:06 +0530

    add TUser and tests

---
+++ test_poetry_github_a/tuser.py
@@ -0,0 +1,12 @@
+import test_poetry_github_B
+
+class TUser():
+
+    def __init__(self, params):
+        pass
+
+    def make_tline(self, params={}):
+        return test_poetry_github_B.TLine(params)
+
+
+
---
+++ tests/test_tuser.py
@@ -0,0 +1,5 @@
+import tuser
+
+def test_tuser():
+    tu = tuser.TUser()
+    line = tu.make_tline()
